ede962e600b7d62a39315b88fb0b295a98675d95
zero323 <mszymkiewicz@gmail.com>
2021-11-23 16:44:43 +0100

    Add build-docs.sh

---
+++ dev/build-docs.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# Copyright 2021 zero323
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+R -e "devtools::document(); pkgdown::build_site()"
+sed -i "s/%>%.html/pipe.html/g" docs/reference/*html
+sed -i "s/%&gt;%.html/pipe.html/g" docs/*html
+sed -i "s/%&gt;%.html/pipe.html/g" docs/articles/*html
